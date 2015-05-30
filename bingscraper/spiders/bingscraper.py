90b29849fef0b6fbe8123583eb9d672e724d879d
Terrile <minzou@live.com>
2015-05-30 13:13:14 +0800

    update bingscrape for pan.baidu.com

--- bingscraper/spiders/bingscraper.py
+++ bingscraper/spiders/bingscraper.py
@@ -44,10 +44,13 @@ class BingSpider(scrapy.Spider):
                 url = item.xpath('.//div[@class="b_title"]/h2/a/@href')
                 caption = item.xpath('.//div[@class="b_caption"]/p')
                 search_res = BingscraperItem()
-                search_res['query'] = query
+                search_res['query'] = query.encode('utf-8')
                 search_res['title'] = title.select('string()').extract()[0]
+                search_res['title'] = search_res['title'].encode('utf-8')
                 search_res['url'] = url.extract()[0]
+                search_res['url'] = search_res['url'].encode('utf-8')
                 search_res['caption'] = caption.select('string()').extract()[0]
+                search_res['caption'] = search_res['caption'].encode('utf-8')
                 search_res['rank'] = rank
                 rank+=1
                 yield  search_res
